3381e364a0c5a5ada95c4aba0a887c142dbafd6a
NHoose <Nikolas.Hoose@gmail.com>
2015-03-17 20:20:19 +0100

    Initial commit

---
+++ app/src/androidTest/java/de/team_eduart/project2ndstage/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.team_eduart.project2ndstage;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
