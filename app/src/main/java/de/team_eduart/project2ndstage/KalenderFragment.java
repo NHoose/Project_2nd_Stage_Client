b482b94722233abad94ddc3d13510b0dbba52603
NHoose <Nikolas.Hoose@gmail.com>
2015-04-25 23:31:27 +0200

    zu viel

---
+++ app/src/main/java/de/team_eduart/project2ndstage/KalenderFragment.java
@@ -0,0 +1,33 @@
+package de.team_eduart.project2ndstage;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class KalenderFragment extends Fragment {
+
+    public KalenderFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kalender, container, false);
+    }
+
+
+
+}
--- app/src/main/java/de/team_eduart/project2ndstage/MainActivity.java
+++ app/src/main/java/de/team_eduart/project2ndstage/MainActivity.java
@@ -1,19 +1,228 @@
 package de.team_eduart.project2ndstage;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mTitle;
+
+    Button HomeButton;
+    Button KalButton;
+    Button PostButton;
+    Button BenachrichtigungenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarView);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        String[] NavDrawerItemsArray = { "Home", "Kalender", "Postfach", "Benachrichtigungen" };
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, NavDrawerItemsArray));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+
+            /** Called when a drawer has settled in a completely closed state. */
+            /*
+            public void onDrawerClosed(View view) {
+
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+            }
+            */
+
+            /** Called when a drawer has settled in a completely open state. */
+            /*
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+            */
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        SwitchToHome();
+
+        HomeButton = (Button) findViewById(R.id.BtnHome);
+        KalButton = (Button) findViewById(R.id.BtnKal);
+        PostButton = (Button) findViewById(R.id.BtnPost);
+        BenachrichtigungenButton = (Button) findViewById(R.id.BtnBenachritungen);
+
+        HomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwitchToHome();
+            }
+        });
+
+        KalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwitchToKal();
+            }
+        });
+
+        PostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwitchToPost();
+            }
+        });
+
+        BenachrichtigungenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwitchToBenachrichtigungen();
+            }
+        });
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu()
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+    */
+
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+
+    private void selectItem(int position) {
+        /* Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.Home_Screen, fragment)
+                .commit();
+
+        */
+
+        switch (position) {
+
+            case 0:
+                mDrawerLayout.closeDrawer(mDrawerList);
+                SwitchToHome();
+                break;
+            case 1:
+                mDrawerLayout.closeDrawer(mDrawerList);
+                SwitchToKal();
+                break;
+            case 2:
+                mDrawerLayout.closeDrawer(mDrawerList);
+                SwitchToPost();
+                break;
+            case 3:
+                mDrawerLayout.closeDrawer(mDrawerList);
+                SwitchToBenachrichtigungen();
+                break;
+        }
+    }
+
+    private void SwitchToHome() {
+        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+        mDrawerList.setItemChecked(0, true);
+
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, homeFragment)
+                .commit();
+    }
+
+    private void SwitchToKal() {
+        Toast.makeText(getApplicationContext(), "Kalender", Toast.LENGTH_SHORT).show();
+        mDrawerList.setItemChecked(1, true);
+
+        KalenderFragment kalFragment = new KalenderFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, kalFragment)
+                .commit();
+    }
+
+    private void SwitchToPost() {
+        Toast.makeText(getApplicationContext(), "Postfach", Toast.LENGTH_SHORT).show();
+        mDrawerList.setItemChecked(2, true);
+    }
+
+    private void SwitchToBenachrichtigungen() {
+        Toast.makeText(getApplicationContext(), "Benachrichtigungen", Toast.LENGTH_SHORT).show();
+        mDrawerList.setItemChecked(3, true);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,6 +240,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
