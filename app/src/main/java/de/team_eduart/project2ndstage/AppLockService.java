89750f1442dd0e44dfe3b8038c90b4bf316787fc
NHoose <Nikolas.Hoose@gmail.com>
2016-01-12 17:01:47 +0100

    alte Aenderungen von Laptop

--- app/src/main/java/de/team_eduart/project2ndstage/AppLockService.java
+++ app/src/main/java/de/team_eduart/project2ndstage/AppLockService.java
@@ -2,11 +2,19 @@ package de.team_eduart.project2ndstage;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,26 +37,78 @@ public class AppLockService extends Service {
         timer.scheduleAtFixedRate(mTimerTask = new TimerTask() {
             public void run() {
 
+                /*
                 ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
                 final ActivityManager.RunningTaskInfo top = am.getRunningTasks(1).get(0);
                 final String packageName = top.topActivity.getPackageName();
+                */
 
-                if (Arrays.asList(blacklistedApps).contains(packageName)) {
+                String TopActivity;
+                if (Build.VERSION.SDK_INT >= 21) {
+                    TopActivity = getTopActivity();
+                } else {
+                    TopActivity = getTopActivityCompat();
+                }
+
+                Log.d("LockService", TopActivity);
+
+                /*
+                if (Arrays.asList(blacklistedApps).contains(TopActivity)) {
+
+                    Toast.makeText(getApplicationContext(), "gesperrte App", Toast.LENGTH_SHORT).show();
+
 
                     Intent launchIntent = new Intent(getBaseContext(), LockedApp.class);
                     launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     getApplication().startActivity(launchIntent);
 
-                }
 
+                }
+                */
             }
         }, 0, 1000);
 
     }
 
+    String getTopActivityCompat() {
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ActivityManager.RunningTaskInfo top = am.getRunningTasks(1).get(0);
+        final String packageName = top.topActivity.getPackageName();
+        return packageName;
+    }
 
-
-
+    String getTopActivity() {
+        String TopActivityName;
+        final int PROCESS_STATE_TOP = 2;
+        ActivityManager.RunningAppProcessInfo currentInfo = null;
+        Field field = null;
+        try {
+            field = ActivityManager.RunningAppProcessInfo.class.getDeclaredField("processState");
+        } catch (Exception ignored) {
+        }
+        ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appList = am.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo app : appList) {
+            if (app.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+                    && app.importanceReasonCode == ActivityManager.RunningAppProcessInfo.REASON_UNKNOWN) {
+                Integer state = null;
+                try {
+                    state = field.getInt(app);
+                } catch (Exception e) {
+                }
+                if (state != null && state == PROCESS_STATE_TOP) {
+                    currentInfo = app;
+                    break;
+                }
+            }
+        }
+        if (currentInfo != null) {
+            TopActivityName = currentInfo.processName;
+        } else {
+            TopActivityName = "error";
+        }
+        return TopActivityName;
+    }
 
 
 
